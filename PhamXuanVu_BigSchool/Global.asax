d3352e74aa9fcc352453ec8a2e765286ca28b04a
_ Zuu <vupham1405@gmail.com>
2022-06-20 06:14:53 +0700

    Add project files.

---
+++ PhamXuanVu_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhamXuanVu_BigSchool.MvcApplication" Language="C#" %>
---
+++ PhamXuanVu_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhamXuanVu_BigSchool.Startup))]
+namespace PhamXuanVu_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
